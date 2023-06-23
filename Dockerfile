de299a97163bfa3fb2c7de31c5536d0f8b1731a1
Andrey Belkin <dkvd1@yandex.ru>
2023-06-23 23:25:31 +0500

    Fixed version JDK and plarform, add tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM --platform=linux/amd64 openjdk:17-jdk-alpine
 EXPOSE 8081
 ADD target/prodapp.jar prodapp.jar
 CMD ["java", "-jar", "prodapp.jar"]
--- src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/ru/netology/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,15 +1,16 @@
 package ru.netology.springbootdemo;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.web.exchanges.HttpExchange;
+import org.springframework.beans.factory.annotation.Autowired;;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
@@ -17,14 +18,15 @@ class SpringBootDemoApplicationTests {
     @Autowired
     TestRestTemplate testRestTemplate;
 
-    private  final GenericContainer<?> devApp = new GenericContainer<>("devapp:0.0.1")
+
+    private final GenericContainer<?> devApp = new GenericContainer<>("devapp:1.0")
             .withExposedPorts(8080);
-    private  final GenericContainer<?> prodApp = new GenericContainer<>("prodapp:0.0.1")
+    private final GenericContainer<?> prodApp = new GenericContainer<>("prodapp:1.0")
             .withExposedPorts(8081);
 
 
     @BeforeEach
-      void setUp() {
+    void setUp() {
         devApp.start();
         prodApp.start();
     }
@@ -34,11 +36,18 @@ class SpringBootDemoApplicationTests {
         Integer devAppPort = devApp.getMappedPort(8080);
         Integer prodAppPort = prodApp.getMappedPort(8081);
 
-        ResponseEntity<String> entityDevApp = testRestTemplate.getForEntity("http://localhost:" + devAppPort, String.class);
-        ResponseEntity<String> entityProdApp = testRestTemplate.getForEntity("http://localhost:" + prodAppPort, String.class);
+        ResponseEntity<String> entityDevApp = testRestTemplate.getForEntity
+                ("http://localhost:" + devAppPort + "/profile", String.class);
+        ResponseEntity<String> entityProdApp = testRestTemplate.getForEntity
+                ("http://localhost:" + prodAppPort + "/profile", String.class);
 
-        System.out.println("DevApp: " + entityDevApp.getBody());
-        System.out.println("ProdApp: " + entityProdApp.getBody());
+        String expectedDev = entityDevApp.getBody();
+        String expectedProd = entityProdApp.getBody();
+
+
+        assertEquals(expectedDev, "Current profile is dev");
+        assertEquals(expectedProd, "Current profile is production");
+
     }
 
 }
